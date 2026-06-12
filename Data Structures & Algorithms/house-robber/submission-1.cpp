bc16b27ab2b41e71412068d9af6faf31d514f964
Arian Boukani <49697930+2arian3@users.noreply.github.com>
2026-06-12 18:03:31 -0400

    Add: house-robber - submission-1

---
+++ Data Structures & Algorithms/house-robber/submission-1.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int r1 = 0;
+        int r2 = 0;
+
+        for (int num: nums) {
+            int temp = max(r2, r1 + num);
+            r1 = r2;
+            r2 = temp;
+        }
+
+        return r2;
+    }
+};
