9b7513b6112cbd7c04cd7f06a70a0e691dff9d14
Arian Boukani <49697930+2arian3@users.noreply.github.com>
2026-07-08 17:59:59 -0400

    Add: clone-graph - submission-0

---
+++ Data Structures & Algorithms/clone-graph/submission-0.cpp
@@ -0,0 +1,50 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* cloneGraph(Node* node) {
+
+        if (!node)
+            return nullptr;
+
+        queue<Node*> nodesToVisit;
+        unordered_map<Node*, Node*> oldToNew;
+
+        oldToNew[node] = new Node(node->val);
+        nodesToVisit.push(node);
+
+        while (!nodesToVisit.empty()) {
+            Node* explore = nodesToVisit.front();
+            nodesToVisit.pop();
+
+            for (auto const n: explore->neighbors) {
+                if (!oldToNew.contains(n)) {
+                    oldToNew[n] = new Node(n->val);
+                    nodesToVisit.push(n);
+                }
+                oldToNew[explore]->neighbors.push_back(oldToNew[n]);
+            }
+        }
+
+        return oldToNew[node];
+    }
+};
