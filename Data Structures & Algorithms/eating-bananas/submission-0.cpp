47c246d6d82bbc661ac6e0ddd17a2543c11aacf0
Arian Boukani <49697930+2arian3@users.noreply.github.com>
2026-08-09 19:17:33 -0400

    Add: eating-bananas - submission-0

---
+++ Data Structures & Algorithms/eating-bananas/submission-0.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int l = 1;
+        int r = *max_element(piles.begin(), piles.end());
+        int minSpeed = r;
+
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            long long totalHours = 0;
+
+            for (auto p: piles)
+                totalHours += ceil((double)(p) / mid);
+
+            if (totalHours <= h) {
+                minSpeed = mid;
+                r = mid - 1;
+            } else
+                l = mid + 1;
+        }
+
+        return minSpeed;
+    }
+};
