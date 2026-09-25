478a509a6b13cdbf744d6ef46ee401bc06878729
Arian Boukani <49697930+2arian3@users.noreply.github.com>
2026-09-25 00:05:10 -0400

    Add: simplify-path - submission-0

---
+++ Data Structures & Algorithms/simplify-path/submission-0.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        stringstream ss(path);
+        string token;
+        vector<string> st;
+
+        while (getline(ss, token, '/')) {
+            if (token == "" || token == ".") {
+                continue;
+            }
+            else if (token == "..") {
+                if (!st.empty()) {
+                    st.pop_back();
+                }
+            } 
+            else {
+                st.push_back(token);
+            }
+        }
+
+        string res = "";
+        for (const auto& dir : st) {
+            res += "/" + dir;
+        }
+
+        return res.empty() ? "/" : res;
+    }
+};
